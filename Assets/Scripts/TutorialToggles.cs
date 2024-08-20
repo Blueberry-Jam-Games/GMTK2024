5f3cde50466f94af9843424b79156344d35ee4cf
CyberGamerX <31047817+CyberGamerX@users.noreply.github.com>
2024-08-19 21:04:39 -0700

    Tutorial art (#46)
    * Changed art
    
    * Started cutscene for tutorial.
    
    * save before pull from main
    
    * save before pull from main
    
    * final tutorial animation

--- Assets/Scripts/TutorialToggles.cs
+++ Assets/Scripts/TutorialToggles.cs
@@ -8,4 +8,6 @@ public static class TutorialToggles
     public static bool DEPTH_WALKING = true;
     public static bool LIGHT_HEIGHT = true;
     public static Action<bool> SetShadowState;
+
+    public static bool LEFT_RIGHT = true;
 }
